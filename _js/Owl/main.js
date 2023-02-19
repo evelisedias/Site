0ae8e6909258457bdcccddffbeb08acde88a4a8b
Evelise Dias <evelisemacieldias@outlook.com>
2023-02-18 22:40:20 -0300

    criando conteudo

---
+++ _js/Owl/main.js
@@ -0,0 +1,14 @@
+/*acao para os botoes iniciais*/
+
+function ouvir(){
+   
+   window.location.href="https://tim.blog/podcast/";
+  
+}
+
+function info(){
+   
+   window.location.href="https://tim.blog/about/";
+  
+}
+
